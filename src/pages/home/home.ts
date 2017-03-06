8ec42d32c2c65c3370cc650132c74ff1f709e5f5
RAZVAN SARCA <razvan.sarca@gmail.com>
2017-03-06 19:39:24 +0200

    basics2

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,19 +2,43 @@ import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
 import { ShopPage } from '../shop/shop';
+import { ContactPage } from '../contact/contact';
+import { AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-  email: string;
-  tel: string;
+  email: string = "";
+  tel: string = "";
+  contact = ContactPage;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController) {};
 
-  }
+  // obligate email or tel to shop
   onClickShopButton() {
-    this.navCtrl.push(ShopPage);
-  }
+    console.log("email: ", this.email);
+    console.log("tel: ", this.tel);
+    
+    if ( this.email == "" && this.tel == "" ) {
+      let alert = this.alertCtrl.create({
+        title: 'Oops',
+        subTitle: 'Please enter either email or phone number!',
+        buttons: ['OK']
+      });
+      alert.present();
+    } else {
+      this.navCtrl.push(ShopPage);
+    }
+  };
+
+  // stop for 1 sec before leaving
+  ionViewCanLeave(): boolean | Promise<{ void }> {
+    const promise = new Promise((resolve, reject) => {
+      setTimeout(resolve(), 5000);
+    });
+    return promise;
+  };
+
 }
